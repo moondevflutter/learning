2d9940b536913d0ec5ab92f0d761b08488e78f4b
Moon Dev Flutter <moondevflutter@gmail.com>
2022-11-16 17:55:32 +0900

    github_client

---
+++ official/desktop_app/github_client/lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '57d79ebb5b175d77f832';
+const githubClientSecret = '545ea78b59f820a6db81d9e2b2c7b68a86b590b0';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
