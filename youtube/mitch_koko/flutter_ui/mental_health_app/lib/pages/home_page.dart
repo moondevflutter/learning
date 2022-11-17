cac3aee6f15d05b37c212da96e68c29ad3252db1
Moon Dev Flutter <moondevflutter@gmail.com>
2022-11-17 17:00:29 +0900

    mitch koko mental_health_app ui

---
+++ youtube/mitch_koko/flutter_ui/mental_health_app/lib/pages/home_page.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+
+import '../util/emoticon_face.dart';
+import '../util/exercise_tile.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[800],
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            // greetings row
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      // Hi, Jared!
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hi, Jared!',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text(
+                            '23 Jan, 2022',
+                            style: TextStyle(
+                              color: Colors.blue[100],
+                              // fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // Notification
+                      Container(
+                        decoration: BoxDecoration(
+                          color: Colors.blue[600],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        padding: EdgeInsets.all(12),
+                        child: Icon(
+                          Icons.notifications,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 25,
+                  ),
+                  // search bar
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Colors.blue[600],
+                        borderRadius: BorderRadius.circular(12)),
+                    padding: EdgeInsets.all(12),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(
+                          'Search',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(
+                    height: 25,
+                  ),
+
+                  // how do you feel
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'How do you feel?',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Icon(Icons.more_horiz, color: Colors.white),
+                    ],
+                  ),
+
+                  SizedBox(
+                    height: 25,
+                  ),
+
+                  // 4 different faces
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      // bad
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '😣'),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Bad', style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      // fine
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '🙂'),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Bad', style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      // well
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '😄'),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Bad', style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                      // excellent
+                      Column(
+                        children: [
+                          EmoticonFace(emoticonFace: '😍'),
+                          SizedBox(
+                            height: 8,
+                          ),
+                          Text('Bad', style: TextStyle(color: Colors.white)),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            Expanded(
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  topRight: Radius.circular(50),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(25),
+                  color: Colors.grey[200],
+                  child: Column(
+                    children: [
+                      // exercise heading
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Exercises',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Icon(Icons.more_horiz),
+                        ],
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      // listview of exercises
+                      Expanded(
+                        child: ListView(
+                          children: [
+                            ExerciseTile(
+                              icon: Icons.favorite,
+                              exerciseName: 'Speaking Skills',
+                              numberOfExercise: 16,
+                              color: Colors.orange,
+                            ),
+                            ExerciseTile(
+                              icon: Icons.person,
+                              exerciseName: 'Reading Skills',
+                              numberOfExercise: 8,
+                              color: Colors.green,
+                            ),
+                            ExerciseTile(
+                              icon: Icons.star,
+                              exerciseName: 'Writing Skills',
+                              numberOfExercise: 20,
+                              color: Colors.pink,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
